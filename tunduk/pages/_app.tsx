f07fd9b77603475c98dfbecaef79bb69a284acdf
tomimarkus991 <tomimarkusalber@gmail.com>
2021-07-21 14:53:20 +0300

    init front end, add chakraUI
    minor changes

---
+++ tunduk/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../styles/theme';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
+export default MyApp;
