935a44b0f672b2b4b739dd0e2450e00091420baa
tomimarkus991 <tomimarkusalber@gmail.com>
2021-07-26 14:07:38 +0300

    add styles

--- tunduk/styles/theme.ts
+++ tunduk/styles/theme.ts
@@ -1,10 +1,14 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
+  beez: {
+    100: '#FBF0E5',
+    500: '#E5E0D5',
+    800: '#2A2828',
+    900: '#DDCDBF',
+  },
+  gray: {
+    800: '#2A2828',
   },
 };
 export const theme = extendTheme({ colors });
