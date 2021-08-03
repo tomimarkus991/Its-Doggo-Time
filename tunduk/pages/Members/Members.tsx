bf64bd7196084273c2cc3b3abb499a20afa286b8
tomimarkus991 <tomimarkusalber@gmail.com>
2021-08-03 08:36:27 +0300

    add remove members functionality and ui

--- tunduk/pages/Members/Members.tsx
+++ tunduk/pages/Members/Members.tsx
@@ -1,10 +1,10 @@
+import { CheckIcon } from '@chakra-ui/icons';
 import {
   Alert,
   AlertDescription,
   AlertIcon,
   AlertTitle,
   Box,
-  Flex,
   HStack,
   IconButton,
   Input,
@@ -16,7 +16,10 @@ import {
   ModalHeader,
   ModalOverlay,
   useDisclosure,
+  VStack,
 } from '@chakra-ui/react';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { AvatarGroup } from '../../components/Avatar';
@@ -38,24 +41,82 @@ import {
 import { getCurrentUser } from '../../utils/getCurrentUser';
 import { supabase } from '../../utils/supabaseClient';
 interface RouteParams {
-  id: string;
+  group_id: string;
 }
 const Members: React.FC = () => {
-  const { id } = useParams<RouteParams>();
+  const { group_id } = useParams<RouteParams>();
+  const { user } = useAuth();
+  const router = useHistory();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const [group_name, setGroupname] = useState<StringOrUndefined>();
   const [group_avatar_url, setGroupAvatarUrl] =
     useState<StringOrUndefined>();
-  const [creator_id, setCreatorId] = useState<StringOrUndefined>();
   const [profiles, setProfiles] = useState<ProfileType[]>();
   const [inviteReceiver, setInviteReceiver] = useState<string | null>();
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [isInvalid, setIsInvalid] = useState(false);
-  const { user } = useAuth();
+  const [creator_id, setCreatorId] = useState<StringOrUndefined>();
 
-  const router = useHistory();
+  const [isInvalid, setIsInvalid] = useState(false);
+  const [isEditable, setIsEditable] = useState(false);
   const [isGroupdataLoading, setIsGroupdataLoading] = useState(true);
   const [isAddMemberDisabled, setIsAddMemberDisabled] =
     useState<boolean>(false);
+
+  const fetchGroupData = async () => {
+    try {
+      setIsGroupdataLoading(true);
+      let { data } = await supabase
+        .from('groups')
+        .select(
+          `
+            id,
+            group_name,
+            avatar_url,
+            creator_id,
+            profiles (id, username, avatar_url)
+        `,
+        )
+        .eq('id', group_id)
+        .single();
+      let _groupData: GroupPageDataType = data;
+      const { avatar_url, group_name, creator_id, profiles } = _groupData;
+
+      setCreatorId(creator_id);
+      setGroupname(group_name);
+      setGroupAvatarUrl(avatar_url);
+      setProfiles(profiles);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setIsGroupdataLoading(false);
+    }
+  };
+
+  const fetchUpdatedMembers = async () => {
+    try {
+      setIsGroupdataLoading(true);
+      let { data, error } = await supabase
+        .from('groups')
+        .select(
+          `
+          id,
+          profiles (id, username, avatar_url)
+        `,
+        )
+        .eq('id', group_id)
+        .single();
+      let _groupData: GroupPageDataType = data;
+      const { profiles } = _groupData;
+
+      setProfiles(profiles);
+
+      if (error) throw error.message;
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setIsGroupdataLoading(false);
+    }
+  };
 
   const sendInvite = async () => {
     try {
@@ -98,10 +159,11 @@ const Members: React.FC = () => {
 
   useEffect(() => {
     const listenForMemberInserts = () => {
-      // when a you are inserted to members
+      // when you are inserted to members
       // (by accepting invite or directly from database)
       supabase
-        .from(`members:group_id=eq.${id}`)
+        // only show it to members with this group_id
+        .from(`members:group_id=eq.${group_id}`)
         .on('INSERT', async payload => {
           let { data: profile } = await supabase
             .from('profiles')
@@ -115,8 +177,6 @@ const Members: React.FC = () => {
             .eq('id', payload.new.profile_id)
             .single();
 
-          console.log('payload.new.profile_id', payload.new.profile_id);
-
           const { id, username, avatar_url } = profile as ProfileType;
 
           const newProfile: ProfileType = {
@@ -129,40 +189,16 @@ const Members: React.FC = () => {
         })
         .subscribe();
     };
-
-    const fetchGroupData = async () => {
-      try {
-        setIsGroupdataLoading(true);
-        let { data, error } = await supabase
-          .from('groups')
-          .select(
-            `
-            id,
-            group_name,
-            avatar_url,
-            creator_id,
-            profiles (id, username, avatar_url)
-        `,
-          )
-          .eq('id', id)
-          .single();
-        let _groupData: GroupPageDataType = data;
-        const { avatar_url, group_name, creator_id, profiles } =
-          _groupData;
-
-        setCreatorId(creator_id);
-        setGroupname(group_name);
-        setGroupAvatarUrl(avatar_url);
-        setProfiles(profiles);
-
-        if (error) throw error.message;
-      } catch (error) {
-        alert(error.message);
-      } finally {
-        setIsGroupdataLoading(false);
-      }
+    const listenForMemberDeletes = () => {
+      supabase
+        .from(`members:group_id=eq.${group_id}`)
+        .on('DELETE', () => {
+          fetchUpdatedMembers();
+        })
+        .subscribe();
     };
     listenForMemberInserts();
+    listenForMemberDeletes();
     fetchGroupData();
   }, []);
 
@@ -177,14 +213,36 @@ const Members: React.FC = () => {
           isLoading={isGroupdataLoading}
           props={{ borderRadius: 100 }}
         >
-          <Flex
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-          >
+          <VStack>
             <AvatarGroup src={group_avatar_url as string} />
             <Name title={group_name} />
-          </Flex>
+            <>
+              {user?.id === creator_id && isEditable === false ? (
+                <IconButton
+                  onClick={() => setIsEditable(true)}
+                  aria-label="Edit"
+                  bgColor="transparent"
+                  _hover={{ bgColor: 'transparent' }}
+                  icon={
+                    <FontAwesomeIcon
+                      icon={faPen}
+                      size={'lg'}
+                      color="#2A2828"
+                    />
+                  }
+                />
+              ) : null}
+              {user?.id === creator_id && isEditable ? (
+                <IconButton
+                  borderRadius="50"
+                  onClick={() => setIsEditable(false)}
+                  aria-label="Save"
+                  colorScheme="green"
+                  icon={<CheckIcon color="white" />}
+                />
+              ) : null}
+            </>
+          </VStack>
         </Skeleton>
       }
       middle={
@@ -200,29 +258,31 @@ const Members: React.FC = () => {
                 />
               </Box>
               <Box pl="24">
-                <Heading title="Members" />
+                <Heading title="Members" fontSize={50} />
               </Box>
             </HStack>
           </Box>
           <MembersContainer
             members={profiles}
+            isEditable={isEditable}
+            group_id={group_id}
+            creator_id={creator_id}
             AddNewMember={
               <>
-                {creator_id === user?.id ? (
-                  <IconButton
-                    onClick={onOpen}
-                    aria-label="Add new doggo group"
-                    size="sm"
-                    w="100%"
-                    h="100%"
-                    p={2}
-                    borderRadius="100"
-                    isDisabled={isAddMemberDisabled}
-                    bgColor="transparent"
-                    _hover={{ bgColor: 'transparent' }}
-                    icon={<AddMemberIcon width="28" height="28" />}
-                  />
-                ) : null}
+                <IconButton
+                  onClick={onOpen}
+                  aria-label="Add new doggo group"
+                  size="sm"
+                  w="100%"
+                  h="100%"
+                  p={2}
+                  borderRadius="100"
+                  isDisabled={isAddMemberDisabled}
+                  bgColor="transparent"
+                  _hover={{ bgColor: 'transparent' }}
+                  icon={<AddMemberIcon width="28" height="28" />}
+                />
+
                 <Modal
                   isOpen={isOpen}
                   onClose={() => {
