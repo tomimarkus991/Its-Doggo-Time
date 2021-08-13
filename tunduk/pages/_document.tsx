5fa5a6245d6a7f0c23e5d5873a92e61946306814
tomimarkus991 <tomimarkusalber@gmail.com>
2021-08-13 15:17:28 +0300

    change theme color

--- tunduk/pages/_document.tsx
+++ tunduk/pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <link rel="shortcut icon" href="/images/favicon.ico" />
-          <meta name="theme-color" content="#FBF0E5" />
+          <meta name="theme-color" content="#000000" />
         </Head>
         {/* <link rel="shortcut icon" href="/favicon.ico" /> */}
         <body>
